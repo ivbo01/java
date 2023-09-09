588580682a90f5e38c17eaf091675977bd21b483
miribvo <irina_makeewa@mail.ru>
2023-09-09 20:41:52 +0000

    num1

---
+++ prak1/num1/Main.java
@@ -0,0 +1,14 @@
+package prak1.num1;
+
+public class Main {
+    public static void main(String args[]) {
+        int[] array = {1, 2, 3};
+        int summ = 0;
+        for (int i = 0; i < array.length; i++) {
+            summ = summ + array[i];
+        }
+        System.out.println(summ);
+        System.out.println(summ/array.length);
+
+    }
+}
