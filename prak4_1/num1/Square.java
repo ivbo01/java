f624ad2f65c01b597e8b72c846818e909002bf49
mirivbo <144561607+mirivbo@users.noreply.github.com>
2023-09-23 20:52:04 +0300

    Create Square.java

---
+++ prak4_1/num1/Square.java
@@ -0,0 +1,25 @@
+// Квадрат
+public class Square extends Shape {
+    private double side;
+
+    public Square(double side) {
+        super("Square");
+        this.side = side;
+    }
+
+    public String getType() {
+        return "Square"; 
+    }
+    
+    public double getArea() {
+        return side * side;
+    }
+
+    public double getPerimeter() {
+        return 4 * side;
+    }
+    
+    public String toString() {
+        return "Square";
+    }
+}
