c06663bf204b24be9df12843c20294054476dadc
mirivbo <144561607+mirivbo@users.noreply.github.com>
2023-09-23 20:51:55 +0300

    Create Rectangle.java

---
+++ prak4_1/num1/Rectangle.java
@@ -0,0 +1,28 @@
+// Прямоугольник
+public class Rectangle extends Shape {
+    private double width;
+    private double height;
+    
+    public Rectangle(double width, double height) {
+        super("Rectangle");
+        this.width = width;
+        this.height = height;
+    }
+
+    public String getType() {
+        return "Rectangle";
+    }
+    
+    public double getArea() {
+        return width * height;
+    }
+    
+    public double getPerimeter() {
+        return 2 * (width + height);
+    }
+    
+    public String toString() {
+        return "Rectangle";
+    }
+    
+}
