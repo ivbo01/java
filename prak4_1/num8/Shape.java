e3d56fc31c9ee060e791b52b454d56ef8d00e318
mirivbo <144561607+mirivbo@users.noreply.github.com>
2023-09-24 20:48:14 +0300

    Update and rename Shape1.java to Shape.java

---
+++ prak4_1/num8/Shape.java
@@ -0,0 +1,31 @@
+// Перепишите суперкласс Shape из задания 1, сделайте его абстрактным и наследуйте подклассы, так как это представлено на UML диаграмме на рис. 4.1 Circle, Rectangle и Square.
+public abstract class Shape {
+    protected String color;
+    protected boolean filled;
+
+    public Shape(String color, boolean filled) {
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    public abstract double getArea();
+
+    public abstract double getPerimeter();
+}
+
