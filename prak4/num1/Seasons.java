d23050a95e1aa9da9aad4adf0f43388f48dcb08e
commit <70198995+voron5096@users.noreply.github.com>
2023-10-07 20:39:42 +0300

    prak4_1

--- prak4/num1/Seasons.java
+++ prak4/num1/Seasons.java
@@ -1,10 +1,10 @@
 // Создать перечисление, содержащее названия времен года.
 public enum Seasons {
     // 3) Перечисление должно содержать переменную, содержащую среднюю температуру в каждом времени года.
-    Зима(-15),   // Средняя температура зимой
-    Весна(18),   // Средняя температура весной
-    Лето(30),   // Средняя температура летом
-    Осень(8);   // Средняя температура осенью
+    Зима(-15), // Средняя температура зимой
+    Весна(18), // Средняя температура весной
+    Лето(30), // Средняя температура летом
+    Осень(8); // Средняя температура осенью
 
     private final int seasonTemp; // Переменная для средней температуры
 
@@ -24,23 +24,7 @@ public enum Seasons {
             return "Холодное время года";
         }
     }
-    
-    public static void main(String[] args) {
-        // 1) Создать переменную, содержащую ваше любимое время года и распечатать всю информацию о нем
-        Seasons favoriteSeason = Seasons.Весна;
-        System.out.println("Мое любимое время года: " + favoriteSeason);
-        
-        // 2) Создать метод, который принимает на вход переменную созданного вами enum типа.
-        // Если значение равно Лето, выводим на консоль “Я люблю лето” и так далее. Используем оператор switch.
-        Seasons iLoveSeason = Seasons.Весна;
-        printFavoriteSeason(iLoveSeason);
-        
-        // 6) В цикле распечатать все времена года, среднюю температуру и описание времени года
-        for (Seasons season: Seasons.values()) {
-            System.out.println(season + ": " + "temp = " + season.getAverageTemperature() + " " + season.getDescription());
-        }
-    }
-    
+
     public static void printFavoriteSeason(Seasons season) {
         switch (season) {
             case Зима:
@@ -55,6 +39,22 @@ public enum Seasons {
             case Осень:
                 System.out.println("Я люблю осень");
                 break;
+        }
+    }
+
+    public static void main(String[] args) {
+        // 1) Создать переменную, содержащую ваше любимое время года и распечатать всю информацию о нем
+        Seasons favoriteSeason = Seasons.Весна;
+        System.out.println("Мое любимое время года: " + favoriteSeason);
+        
+        // 2) Создать метод, который принимает на вход переменную созданного вами enum типа.
+        // Если значение равно Лето, выводим на консоль “Я люблю лето” и так далее. Используем оператор switch.
+        Seasons iLoveSeason = Seasons.Весна;
+        printFavoriteSeason(iLoveSeason);
+        
+        // 6) В цикле распечатать все времена года, среднюю температуру и описание времени года
+        for (Seasons season: Seasons.values()) {
+            System.out.println(season + ": " + "температура = " + season.getAverageTemperature() + " " + season.getDescription());
         }
     }
 }
